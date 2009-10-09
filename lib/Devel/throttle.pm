89d9d9b319e6dff27619fd7ddcd40280e387524f
Guido Flohr <guido.flohr@cantanea.com>
2009-10-09 15:10:30 +0300

    Minimum infrastructure for CPAN and git.

---
+++ lib/Devel/throttle.pm
@@ -0,0 +1,15 @@
+#! /bin/false
+
+# This file is part of Devel-throttle.
+# Copyright (C) 2009 Guido Flohr <guido@imperia.net>, 
+# all rights reserved.
+
+# This program is free software; you can redistribute it and/or modify it
+# under the same terms as Perl itself.
+
+package Devel::throttle;
+
+use vars qw ($VERSION);
+$VERSION = '0.01';
+
+1;
